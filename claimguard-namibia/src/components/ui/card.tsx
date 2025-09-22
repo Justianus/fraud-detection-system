739609c4171c27adff9b9f29bcff33ad7596f1ce
Justianus <justianuskanyinga@gmail.com>
2025-09-22 19:55:32 +0200

    Refactor: Inline cn function in alert, button, and card components to eliminate import dependency

--- claimguard-namibia/src/components/ui/card.tsx
+++ claimguard-namibia/src/components/ui/card.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)} {...props} />
